8645cf4f93269ac0ac61a3b095a2a74d724f91cb
Jorge Rodriguez <jorgejmrv6@gmail.com>
2021-02-28 17:35:57 -0300

    Agrega seed de permisos

---
+++ permits-seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.permits(permit_id, name, display_name) VALUES (1, 'CREATE_ROLE', 'Creación de Rol');
+INSERT INTO public.permits(permit_id, name, display_name) VALUES (2, 'EDIT_ROLE', 'Edición de Rol');
+INSERT INTO public.permits(permit_id, name, display_name) VALUES (3, 'CREATE_USER', 'Creación de Usuario');
+INSERT INTO public.permits(permit_id, name, display_name) VALUES (4, 'EDIT_USER', 'Edición de Usuario');
