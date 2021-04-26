6d0237da714f9156929915f4d4b12afa37f83aa4
ovianconi <ovianconi@gmail.com>
2021-04-26 14:16:46 -0400

    Se agrega permiso para configuraciones

--- permits-seed.sql
+++ permits-seed.sql
@@ -2,3 +2,4 @@ INSERT INTO public.permits(permit_id, name, display_name) VALUES (1, 'ROLE_MANAG
 INSERT INTO public.permits(permit_id, name, display_name) VALUES (2, 'USER_MANAGER', 'Gestor Usuarios');
 INSERT INTO public.permits(permit_id, name, display_name) VALUES (3, 'PROJECT_MANAGER', 'Gestor Proyectos');
 INSERT INTO public.permits(permit_id, name, display_name) VALUES (4, 'TASK_MANAGER', 'Gestor Tareas');
+INSERT INTO public.permits(permit_id, name, display_name) VALUES (5, 'CONFIGURATION_MANAGER', 'Gestor Configuraciones');
